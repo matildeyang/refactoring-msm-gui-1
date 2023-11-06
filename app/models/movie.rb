eff340d252651f1edb4c2229da6497d5b21a2359
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-11-06 21:43:30 +0000

    movie and director methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,18 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def title_with_year
+    return "#{self.title} (#{self.year})"
+  end
+
+  def director
+    my_director_id = self.director_id
+
+    matching_directors = Director.where({ :id => my_director_id })
+    
+    the_director = matching_directors.at(0)
+
+    return the_director
+  end
+  
 end
